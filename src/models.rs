a0cd3781dc2c201e7814784e08ecf95841033063
Lane Sawyer <github@lanesawyer.dev>
2020-08-14 00:02:06 -0600

    fix: Error

--- src/models.rs
+++ src/models.rs
@@ -80,7 +80,7 @@ pub struct Params {
 impl From<&User> for Params {
     fn from(user: &User) -> Self {
         Params {
-            pw_cost: user.pw_cost.clone(),
+            pw_cost: user.pw_cost,
             pw_nonce: user.pw_nonce.clone(),
             version: user.version.clone(),
         }
